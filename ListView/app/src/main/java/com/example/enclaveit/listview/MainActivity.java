83e5f701598d8061fb3ba1aaf900a1024c7a3d62
danisluis6 <vuongluis@hotmail.com>
2017-01-03 14:19:17 +0700

    ok

---
+++ ListView/app/src/main/java/com/example/enclaveit/listview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.enclaveit.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+http://www.androidhive.info/2012/02/android-custom-listview-with-image-and-text/
